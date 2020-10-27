1ec12e568f4cfeecbeb2ee2a32d1cd033cd386ce
David John <davidajohn@gmail.com>
2020-10-27 01:09:40 +0000

    Added a mutation to create a new link

--- src/index.js
+++ src/index.js
@@ -6,16 +6,24 @@ let links = [{
   description: 'Hacker News'
 }]
 
+let idCount = links.length // rudimentary counter variable
+
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
     feed: () => links,
   },
-  Link: {
-    id: (parent) => parent.id,
-    description: (parent) => parent.description,
-    url: (parent) => parent.url,
-  }
+  Mutation: {
+    post: (parent, args) => {
+       const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    }
+  },
 }
 
 const server = new GraphQLServer({
